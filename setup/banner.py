0d3d57156aa3a5454160d0254f527aa7ada79974
hacker6291 <98105494+hacker6291@users.noreply.github.com>
2023-10-25 13:48:06 +0530

    Update banner.py

--- setup/banner.py
+++ setup/banner.py
@@ -16,7 +16,7 @@ logo = f"""
 | |___| | | |  __/ (__|   <  __/ |
  \____|_| |_|\___|\___|_|\_\___|_|
  
-                             {c + "coded By: QADIR"+y +" "}             
+                             {c + "coded By: cybershielddefender"+y +" "}             
                                                                                                                    
 """
 c = colors
